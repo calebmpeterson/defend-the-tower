73c7b63d202ebec035628c474d086a05bfecb6cc
Caleb Peterson <caleb.peterson@cubicle6.com>
2025-08-09 16:42:09 -0400

    give enemies a bit of organic wobble to their appearance

--- src/entities/Enemy.tsx
+++ src/entities/Enemy.tsx
@@ -1,32 +1,61 @@
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
 import memoize from "micro-memoize";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import type { Enemy as EnemyProps } from "../types";
 import { position } from "../utils/Geometry";
+
+const wobblePulse = keyframes`
+  0%   { transform: scale(1) rotate(0deg); }
+  25%  { transform: scale(1.05) rotate(0.5deg); }
+  50%  { transform: scale(0.97) rotate(-0.5deg); }
+  75%  { transform: scale(1.07) rotate(0.3deg); }
+  100% { transform: scale(1) rotate(0deg); }
+`;
 
 const enemyCss = memoize(
   (size: number, color: string) => css`
     position: absolute;
     width: ${size}px;
     height: ${size}px;
-    border: 1px solid var(${color});
-    box-sizing: border-box;
-    border-radius: ${size / 2}px;
-    box-shadow: 0 0 ${size / 3}px 1px rgba(var(${color}), 0.6);
+
+    & > [data-appearance] {
+      width: ${size}px;
+      height: ${size}px;
+      border: 1px solid var(${color});
+      box-sizing: border-box;
+      border-radius: ${size / 2}px;
+      box-shadow: 0 0 ${size / 3}px 1px rgba(var(${color}), 0.6);
+
+      animation-name: ${wobblePulse};
+      animation-duration: 500;
+      animation-timing-function: ease-in-out;
+      animation-delay: 0s;
+      animation-iteration-count: infinite;
+      animation-direction: normal;
+      animation-fill-mode: none;
+      animation-play-state: running;
+    }
   `
 );
 
-const enemyStyle = (props: EnemyProps, isInRange: boolean) => ({
+const positionStyle = (props: EnemyProps, isInRange: boolean) => ({
   ...position(props.position.x, props.position.y, props.size),
   opacity: isInRange ? 1 : 0.5,
 });
 
+const appearanceStyle = (animationDuration: number) => ({
+  animationDuration: `${300 + animationDuration * 200}ms`,
+});
+
 const Enemy: FC<EnemyProps> = (props) => {
+  const duration = useMemo(() => Math.random(), []);
   return (
     <div
       css={enemyCss(props.size, props.color)}
-      style={enemyStyle(props, true)}
-    />
+      style={positionStyle(props, true)}
+    >
+      <div data-appearance style={appearanceStyle(duration)} />
+    </div>
   );
 };
 
--- src/entities/Pointer.tsx
+++ src/entities/Pointer.tsx
@@ -1,13 +1,13 @@
 import { css } from "@emotion/react";
 import { FC } from "react";
-import { selector, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import { pointerPositionState } from "../input";
 import { towerPositionState } from "../state/tower";
 import { position } from "../utils/Geometry";
 import { distance } from "../utils/Trigonometry";
 import { TOWER_SIZE } from "./Tower";
 
-const SIZE = 3;
+const SIZE = 5;
 
 const pointerCss = css`
   position: absolute;
@@ -17,22 +17,14 @@ const pointerCss = css`
   border-radius: ${SIZE}px;
   background-color: rgb(var(--fg));
   box-shadow: 0 0 3px 1px rgba(var(--fg), 0.33);
-  opacity: 0.75;
+  opacity: 0.9;
 `;
-
-const isVisibleState = selector<boolean>({
-  key: "pointer/isVisible",
-  get: ({ get }) => {
-    const pointerPosition = get(pointerPositionState);
-    const towerPosition = get(towerPositionState);
-
-    return distance(pointerPosition, towerPosition) > TOWER_SIZE;
-  },
-});
 
 const Pointer: FC = () => {
   const pointerPosition = useRecoilValue(pointerPositionState);
-  const isVisible = useRecoilValue(isVisibleState);
+  const towerPosition = useRecoilValue(towerPositionState);
+
+  const isVisible = distance(pointerPosition, towerPosition) > TOWER_SIZE;
   return (
     <div
       css={pointerCss}
